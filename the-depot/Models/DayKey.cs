e62299f1456df8fb003dbb78e48ae9551feb31ea
Ruben-Molenaar <Ruben@appinnovators.nl>
2023-03-20 19:02:49 +0100

    UsedOnDate is now a datetime

--- the-depot/Models/DayKey.cs
+++ the-depot/Models/DayKey.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 using the_depot.Constants;
 
 namespace the_depot.Models
 {
     public class DayKey
     {
-        public string Key { get; set; }
+        public string Key { get; set; } = string.Empty;
 
         public bool Used { get; set; }
 
-        public string UsedOnDate { get; set; }
+        public DateTime UsedOnDate { get; set; }
 
         public Roles Role { get; set; }
     }
--- the-depot/Program.cs
+++ the-depot/Program.cs
@@ -10,11 +10,11 @@ namespace ConsoleApp
 {
     class Program
     {
-        public static bool TryKey(string key, out DayKey dayKey, out string error)
+        public static bool TryKey(string key, out DayKey? dayKey, out string error)
         {
             error = "";
             var json = File.ReadAllText("../../../Keys.json");
-            List<DayKey> dayKeys = JsonSerializer.Deserialize<List<DayKey>>(json);
+            List<DayKey> dayKeys = JsonSerializer.Deserialize<List<DayKey>>(json) ?? new List<DayKey>();
             dayKey = dayKeys.FirstOrDefault(x => x.Key == key);
             if (dayKey == null)
             {
@@ -23,7 +23,7 @@ namespace ConsoleApp
             }
             if (dayKey.Used)
             {
-                error = $"Deze code is al gebruikt op {DateTime.Parse(dayKey.UsedOnDate).ToString("dd-MM-yyyy HH:mm")} om een reservering te maken, annuleer eerst deze reservering om een andere reservering te kunnen plaatsen";
+                error = $"Deze code is al gebruikt op {dayKey.UsedOnDate.ToString("dd-MM-yyyy HH:mm")} om een reservering te maken, annuleer eerst deze reservering om een andere reservering te kunnen plaatsen";
                 return false;
             }
             return true;
