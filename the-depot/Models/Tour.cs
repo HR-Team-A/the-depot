95c15eaa301bc257c040346405035f9934441b22
jimtronchet <jim@web-mine.nl>
2023-04-24 15:55:05 +0200

    use tab

--- the-depot/Models/Tour.cs
+++ the-depot/Models/Tour.cs
@@ -3,7 +3,7 @@ using the_depot.Constants;
 
 namespace the_depot.Models
 {
-	public class Tour
+    public class Tour
 	{
         public DateTime Time { get; set; }
         public int MaxAttendees { get; set; }
